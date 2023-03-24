d85b791cc1ea5020ae9b566c31a781a27e347a56
Muhammad Hassan Abbas <hassanabbas4745@gmail.com>
2023-03-25 00:13:28 +0500

    completed till 2:42:00

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  @UseGuards(AuthGuard('jwt'))
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log({ email });
+
+    return user;
   }
 }
